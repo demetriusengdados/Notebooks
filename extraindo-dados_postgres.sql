edac9bfc6162d289133f3f84308c6713ab94309e
demetriusengdados <dimagela25@gmail.com>
2023-08-02 10:20:30 -0300

    Curso Spark com pyspark

---
+++ extraindo-dados_postgres.sql
@@ -0,0 +1,20 @@
+create database vendas;
+
+CREATE DATABASE;
+
+\i /C:/Usuários/Demetrius/Downloads/demo/1.CreateTable.sql
+
+\i /C:/Usuários/Demetrius/Downloads/demo/2.InsertIntoCliente.sql
+
+\i /C:/Usuários/Demetrius/Downloads/demo/3.InsertIntoProduto.sql
+
+\i /C:/Usuários/Demetrius/Downloads/demo/4.InsertIntoVendedores.sql
+
+\i /C:/Usuários/Demetrius/Downloads/demo/1.InsertIntoVendas.sql
+
+\i /C:/Usuários/Demetrius/Downloads/demo/6.InsertIntoItensVenda.sql
+
+select * from clientes;
+
+select * from vendedores;
+
