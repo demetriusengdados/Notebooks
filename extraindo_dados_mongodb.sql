bc4c8fdb703492258166db0ecd5688e66ee1283c
demetriusengdados <dimagela25@gmail.com>
2023-08-08 09:38:55 -0300

    Curso Spark com pyspark

--- extraindo_dados_mongodb.sql
+++ extraindo_dados_mongodb.sql
@@ -1,6 +1,6 @@
 mongo import --db post --collection post --legacy --file c://Usuários/Demetrius/Downloads/mongo/posts.json (comando para startar o mongo e o caminho onde o arquivo está)
 
-show dbs 
+show dbs (expondo os bancos existentes)
 
 use post (comando para setar o banco a ser usado)
 
@@ -13,4 +13,6 @@ pyspark --packages org.mongodb.spark:mongo-spark-connector_2.12.3.0.1 (extraindo
 posts = spark.read.format("mongo").option("uri","mongodb://127.0.0.1/posts.post").load()
 
 posts.show()
+
+posts.write.format("mongo").option("uri","mongodb://127.0.0.1/posts2.post").save()
 
