b735b0655c9e634ff0b693643803954c28172610
demetriusengdados <dimagela25@gmail.com>
2023-08-08 09:14:10 -0300

    Curso Spark com pyspark

--- extraindo-dados_postgres.sql
+++ extraindo-dados_postgres.sql
@@ -18,3 +18,33 @@ select * from clientes;
 
 select * from vendedores;
 
+#caminho para o arquivo do banco postgres /Downloads pyspark --jars postgresql-42.2.23.jar (comando para executar o banco no pyspark)
+
+from pyspark import SparkSession
+
+resumo = spark.read.format('jdbc').option("url",'jdbc:postgresql://localhost:5432/vendas')
+    .option("dbtable","Vendas")
+    .option('user','postgres')
+    .option("password",'123456')
+    .option('driver', 'org.postgresql.Driver').load()
+
+resumo.show()
+
+clientes = spark.read.format('jdbc').option("url",'jdbc:postgresql://localhost:5432/vendas')
+    .option("dbtable","Clientes")
+    .option('user','postgres')
+    .option("password",'123456')
+    .option('driver', 'org.postgresql.Driver').load()
+
+cliente.show()
+
+vendadata = resumo.select("data", 'total')
+vendadata.show()
+
+vendadata.write.format('jdbc').option("url",'jdbc:postgresql://localhost:5432/vendas')
+    .option("dbtable","vendadata")
+    .option('user','postgres')
+    .option("password",'123456')
+    .option('driver', 'org.postgresql.Driver').save()
+
+select * from vendadata
